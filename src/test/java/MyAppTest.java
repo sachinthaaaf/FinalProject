8e280107403eae0b4216a35f04ab024c497f074d
Sachintha Fernando <fernandosachintha08@gmail.com>
2024-09-29 21:24:56 +0530

    Create MyAppTest.java

---
+++ src/test/java/MyAppTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+public class MyAppTest {
+
+    @Test
+    public void testApp() {
+        // A simple test that always passes
+        assertTrue(true);
+    }
+}

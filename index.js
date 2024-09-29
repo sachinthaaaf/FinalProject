3cefb921d27d5c3ca7cb2e66da0f92a585ef3ce0
Sachintha Fernando <fernandosachintha08@gmail.com>
2024-09-29 21:49:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,31 +5,35 @@ const path = require('path');
 // Define the port your server will listen on
 const port = 3000;
 
-
 const server = http.createServer((req, res) => {
     if (req.url === '/') {
+        // Serve plain text for the root URL
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('Hello World!\n');
     } else if (req.url === '/status') {
+        // Serve status response
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.end('Status: OK');
+    } else if (req.url === '/index.html') {
+        // Serve the index.html file if the request is for it
+        const filePath = path.join(__dirname, 'index.html');
+        
+        fs.readFile(filePath, (err, content) => {
+            if (err) {
+                // If there's an error, return a 500 Internal Server Error
+                res.writeHead(500, { 'Content-Type': 'text/plain' });
+                res.end('500 - Internal Server Error');
+            } else {
+                // If successful, serve the index.html file
+                res.writeHead(200, { 'Content-Type': 'text/html' });
+                res.end(content, 'utf-8');
+            }
+        });
     } else {
+        // For any other URL, return a 404 Not Found
         res.writeHead(404, { 'Content-Type': 'text/plain' });
         res.end('404 Not Found');
     }
-
-  // Read the index.html file
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      // If there's an error, return a 500 Internal Server Error
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.end('500 - Internal Server Error');
-    } else {
-      // If successful, serve the index.html file
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      res.end(content, 'utf-8');
-    }
-  });
 });
 
 // Start the server and listen on the defined port

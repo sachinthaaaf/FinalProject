f46168f036adbc81f661bc15376a87e16ec98074
Sachintha Fernando <fernandosachintha08@gmail.com>
2024-09-29 20:38:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-const express = require('express');
-const app = express();
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+// Define the port your server will listen on
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Create an HTTP server
+const server = http.createServer((req, res) => {
+  // Set the file path to the index.html file
+  const filePath = path.join(__dirname, 'index.html');
+
+  // Read the index.html file
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      // If there's an error, return a 500 Internal Server Error
+      res.writeHead(500, { 'Content-Type': 'text/plain' });
+      res.end('500 - Internal Server Error');
+    } else {
+      // If successful, serve the index.html file
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content, 'utf-8');
+    }
+  });
 });
 
-app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`);
+// Start the server and listen on the defined port
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
 });
